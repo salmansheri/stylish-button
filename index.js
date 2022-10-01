f30a893f3426c20a513105b75e470ffb1ace3cbd
salmansheri <sheriffsalman00@gmail.com>
2022-10-01 21:09:28 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("mouseover", function (event) {
+  const x = (event.pageX - btnEl.offsetLeft);
+  const y = (event.pageY - btnEl.offsetTop);
+   
+  btnEl.style.setProperty("--xPos", x + "px"); 
+  btnEl.style.setProperty("--yPos", y + "px"); 
+  
+
+
+   
+
+});
